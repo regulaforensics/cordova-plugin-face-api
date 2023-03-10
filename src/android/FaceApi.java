9babff2455a53b3117cbc68e7d2918ddc3739f9f
Jenkins <jenkins@regula.by>
2023-03-10 18:44:13 +0300

    5.1.2

--- src/android/FaceApi.java
+++ src/android/FaceApi.java
@@ -190,6 +190,8 @@ public class FaceApi extends CordovaPlugin {
 
     private void presentFaceCaptureActivityWithConfig(Callback callback, JSONObject config) throws JSONException {
         FaceCaptureConfiguration.Builder builder = new FaceCaptureConfiguration.Builder();
+        if (config.has("copyright"))
+            builder.setCopyright(config.getBoolean("copyright"));
         if (config.has("cameraId"))
             builder.setCameraId(config.getInt("cameraId"));
         if (config.has("cameraSwitchEnabled"))
@@ -207,6 +209,8 @@ public class FaceApi extends CordovaPlugin {
 
     private void startLivenessWithConfig(Callback callback, JSONObject config) throws JSONException {
         LivenessConfiguration.Builder builder = new LivenessConfiguration.Builder();
+        if (config.has("copyright"))
+            builder.setCopyright(config.getBoolean("copyright"));
         if (config.has("attemptsCount"))
             builder.setAttemptsCount(config.getInt("attemptsCount"));
         if (config.has("sessionId"))
