755b8ba944cce4ad94135e354643fa1d8076864e
Jenkins <jenkins@regula.by>
2023-07-14 10:57:25 +0300

    0.0.76

--- src/ios/RFSWFaceApi.m
+++ src/ios/RFSWFaceApi.m
@@ -232,7 +232,7 @@ static CDVInvokedUrlCommand* _command;
         if([config valueForKey:@"recordingProcess"] != nil)
             [builder setRecordingProcessEnabled:[[config valueForKey:@"recordingProcess"] boolValue]];
         if([config valueForKey:@"closeButtonEnabled"] != nil)
-            [builder setCloseButtonEnabled:[self RFSCameraPositionWithNSInteger:[[config valueForKey:@"closeButtonEnabled"] integerValue]]];
+            [builder setCloseButtonEnabled:[[config valueForKey:@"closeButtonEnabled"] boolValue]];
         if([config valueForKey:@"tag"] != nil)
             [builder setTag:[config valueForKey:@"tag"]];
         if([config valueForKey:@"skipStep"] != nil) {
